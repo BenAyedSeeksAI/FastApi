8acdecc53b63f628e526c275d09b6eb6b62d7e3f
Mohamed Fares Ben ayed <faresaction@yahoo.fr>
2022-08-12 13:54:44 +0200

    Initial commit FastApi

---
+++ blade.py
@@ -0,0 +1,23 @@
+import imp
+from fastapi import FastAPI
+
+app = FastAPI()
+# def welcome():
+#     return 'Hello world'
+
+@app.get("/")
+def getData():
+    data = {"name" : "Fares",
+            "age" : "24"
+            }
+    return data
+def HelloWorld():
+    return 'Hello world'
+
+
+
+
+
+
+
+
